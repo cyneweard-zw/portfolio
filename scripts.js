035e229a2c63565d63510e1ce83326a18fb04e53
cyneweard-zw <cyneweard@outlook.com>
2024-10-16 13:05:39 +0200

    Update

---
+++ scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    gsap.registerPlugin(
+        Flip,
+        ScrollTrigger,
+        Observer,
+        ScrollToPlugin,
+        Draggable,
+        MotionPathPlugin,
+        EaselPlugin,
+        PixiPlugin,
+        TextPlugin,
+        RoughEase,
+        ExpoScaleEase,
+        SlowMo,
+        CustomEase
+    );
+
+    // GSAP code here!
+});
