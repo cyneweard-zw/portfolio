b2c955d5cc7461be15670c859674806ca9f41c7b
cyneweard-zw <cyneweard@outlook.com>
2024-10-24 15:00:22 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("Contact link as a card added!");
+document.addEventListener("DOMContentLoaded", (event) => {
+    // Register GSAP plugins
+    gsap.registerPlugin(
+      Flip,
+      ScrollTrigger,
+      Observer,
+      ScrollToPlugin,
+      Draggable,
+      MotionPathPlugin,
+      EaselPlugin,
+      PixiPlugin,
+      TextPlugin,
+      RoughEase,
+      ExpoScaleEase,
+      SlowMo,
+      CustomEase
+    );
+  
+    // Add any GSAP animations or ScrollTrigger logic here
+    console.log("GSAP plugins registered successfully!");
+  });
+  
